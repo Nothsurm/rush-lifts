91e8cab05692b72151e8d2fe3869eddac1241328
Mike Rushton <mrush94ton@gmail.com>
2024-04-20 21:20:24 +0100

    created first user

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import connectDB from './config/DB';
 import cookieParser from 'cookie-parser'
+import userRoutes from './routes/userRoutes'
 
 dotenv.config()
 connectDB();
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.use('/api/users', userRoutes)
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
---
+++ backend/utils/createToken.ts
@@ -0,0 +1,18 @@
+import { Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res: Response, userId: Object) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET as string, {expiresIn: '30d'});
+
+    //Set JWT as an http-only cookie
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'development',
+        sameSite: 'strict',
+        maxAge: 30* 24 * 60 * 60 * 1000
+    })
+
+    return token
+};
+
+export default generateToken;
