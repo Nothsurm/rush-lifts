7eb18a6f78bfa6afdcae2279ea7feba14ce3893c
Mike Rushton <mrush94ton@gmail.com>
2024-05-22 19:28:16 +0100

    added upload profile image functionality to firebase

--- frontend/src/pages/protected/Profile.tsx
+++ frontend/src/pages/protected/Profile.tsx
@@ -11,7 +11,11 @@ import {
 } from "../../components/ui/form"
 import { Input } from "../../components/ui/input"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 import { Link, useNavigate } from "react-router-dom"
@@ -20,6 +24,7 @@ import { useLoginMutation } from "@/redux/api/usersApiSlice"
 import { useDispatch, useSelector } from "react-redux"
 import { signInStart, signInSuccess, signInFailure } from "@/redux/features/auth/authSlice"
 import { toast } from "sonner"
+import { app } from "@/firebase"
 
 const formSchema = z.object({
     username: z.string().min(5, {
@@ -37,6 +42,11 @@ export type UserData = z.infer<typeof formSchema>
 
 export default function Register() {
   const [visible, setVisible] = useState(false)
+  const [imageFile, setImageFile] = useState<any>(null)
+  const [imageFileUrl, setImageFileUrl] = useState<string | null>(null)
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState<any>(null)
+  const [imageFileUploading, setImageFileUploading] = useState(false)
+  const filePickerRef = useRef<HTMLInputElement>(null)
   const [login, {isLoading}] = useLoginMutation()
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -79,14 +89,89 @@ export default function Register() {
     }
   };*/
 
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.[0]
+    if (file) {
+        setImageFile(file)
+        setImageFileUrl(URL.createObjectURL(file)) //retrieves image url from users pc
+    }
+}
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage()
+    }
+  }, [imageFile])
+
+  const uploadImage = async() => {
+    setImageFileUploading(true)
+    const storage = getStorage(app)
+    const fileName = new Date().getTime() + imageFile?.name
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, imageFile)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0))
+      },
+      (error) => {
+        toast.error('Could not upload image (File must be less than 2MB)')
+        setImageFileUploadProgress(null)
+        setImageFile(null)
+        setImageFileUrl(null)
+        setImageFileUploading(false)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL)
+          setImageFileUploading(false)
+          toast.success('Image upload successfull')
+        })
+      }
+    )
+  }
+
   return (
     <div className="max-w-96 mx-auto mt-20">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <div className="flex flex-col gap-4">
             <p className='flex justify-center font-semibold text-3xl'>Profile</p>
-            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-                <img src={currentUser.profilePicture} alt="" className="rounded-full w-full h-full border-4 border-[lightgray] object-cover" />
+            <input 
+              type="file" 
+              accept='image/*' 
+              onChange={handleImageChange} 
+              ref={filePickerRef}
+              hidden
+            />
+            <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+              onClick={() => filePickerRef?.current?.click()}  
+            >
+              {imageFileUploadProgress && (
+                <CircularProgressbar 
+                  value={imageFileUploadProgress || 0} 
+                  text={`${imageFileUploadProgress}%`} 
+                  strokeWidth={5}
+                  styles={{
+                    root: {
+                      width: '100%',
+                      height: '100%',
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                    },
+                    path: {
+                      stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100})`
+                    }
+                  }}
+                />
+              )}
+                <img 
+                  src={imageFileUrl || currentUser.profilePicture} 
+                  className={`rounded-full w-full h-full border-4 border-[lightgray] object-cover ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'}`} 
+
+                />
             </div>
         </div>
         <FormField
